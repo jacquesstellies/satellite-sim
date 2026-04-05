72e4a519f996848cea533915015d822359cad665
Jacques <jacques@umantec.net>
2026-04-05 16:25:50 +0200

    update magt with momentum dumping simplified

--- src/magt.py
+++ src/magt.py
@@ -7,6 +7,7 @@ EARTH_MAG_DIPOLE = 7.96e15
 class MagtModule():
     config : dict = None
     orbit : Orbit = None
+    T = np.zeros(3)
 
     B_field = np.zeros(3)
     def __init__(self, config, orbit):
@@ -24,16 +25,26 @@ class MagtModule():
         self.B_field = EARTH_MAG_DIPOLE / (self.orbit.radius)**3 * np.array([np.cos(alpha-eta_m)*np.sin(xi_m),
                                                                         np.cos(xi_m),
                                                                         -2*np.sin(alpha-eta_m)*np.sin(xi_m)])
-    
-    def calc_torque(self, q_err, w_sat, t):
+    prev = 0
+    def calc_torque(self, q_err, w_sat, H_sat, t):
         ## Calculate magnetic torque
-        self.calc_B_field(t)  # t should be passed appropriately
-        m = np.ones(3)*self.config['magt']['mag_moment_max']
-        T_magt = np.array([[0, self.B_field[2], -self.B_field[1]], [-self.B_field[2], 0, self.B_field[0]], [self.B_field[1], -self.B_field[0], 0]]) @ m
-        T_magt[0], T_magt[1] = 0, 0
-        k = 1
-        T_magt[2] = T_magt[2] * -1 * my_utils._sign(w_sat[2]) * k
-        # T_magt[2] = T_magt[2] * -1 * my_utils._sign(q_err[2]) * k
+        # self.calc_B_field(t)  # t should be passed appropriately
+        # m = np.ones(3)*self.config['magt']['mag_moment_max']
+        # T_magt = np.array([[0, self.B_field[2], -self.B_field[1]], [-self.B_field[2], 0, self.B_field[0]], [self.B_field[1], -self.B_field[0], 0]]) @ m
+        # T_magt[0], T_magt[1] = 0, 0
+        # control_type = "z-axis_simple"
+        control_type = "momentum_dumping"
+        match control_type:
+            case "z-axis_simple":
+                self.T = np.zeros(3)
+                k = 1
+                self.T[2] = -1 * my_utils._sign(w_sat[2]) * k
+                self.T[2] = my_utils.low_pass_filter(self.T[2], self.prev, 0.5)
+            case "momentum_dumping":
+                m = np.ones(3)*self.config['magt']['mag_moment_max']
+                k = 1
+                self.T = my_utils.sat_vec(k*H_sat, 0.01)
+        # T_magt[2] = T_magt[2] * my_utils._sign(q_err[2]) * k
 
         ## Testing Stuff
         # T_magt = np.cross(m, self.B_field)
@@ -45,4 +56,3 @@ class MagtModule():
 
         # T_magt = np.zeros(3)
         # T_magt[2] = 10 * 1e-3 * -1*my_utils._sign (w_sat[2])
-        return T_magt
