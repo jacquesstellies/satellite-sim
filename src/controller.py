Fix quaternion error calcualtion mistake Nadafi

src/controller.py
@@ -50,7 +50,7 @@ class NadafiController:
         self.lambda_2 = Nadafi_config['lambda_2']
         self.lambda_3 = Nadafi_config['lambda_3']
         # self.Lambda = np.diag([self.lambda_1, self.lambda_2, self.lambda_3])
-        self.a = self.lambda_1 - self.lambda_3
+        self.a = self.lambda_2 - self.lambda_3
         self.b = self.lambda_3 - self.lambda_1
         self.t_sample = config['controller']['t_sample']
         if config['simulation']['verbose'] is True:

src/my_utils.py
@@ -198,7 +198,7 @@ def get_quaternion_error_bong_wie(qc : np.quaternion, qd : np.quaternion):
 def get_quaternion_error_Nadafi(qd : np.quaternion, q : np.quaternion):
 
     qe = np.array([[qd.w, qd.x, qd.y, qd.z],
-                   [qd.x, -qd.w, -1*qd.z, -1*qd.y],
+                   [qd.x, -1*qd.w, -1*qd.z, qd.y],
                    [qd.y, qd.z, -1*qd.w, -1*qd.x],
                    [qd.z, -1*qd.y, qd.x, -1*qd.w]])\
         @ np.array([q.w, q.x, q.y, q.z])